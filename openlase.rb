413affcce41eed96ac8bd8669c09a46a45fabbe7
TT <edy555@gmail.com>
2013-03-04 20:23:11 +0900

    add formula

--- openlase.rb
+++ openlase.rb
@@ -1,1 +1,23 @@
-/usr/local/Library/Formula/openlase.rb
+require 'formula'
+
+# Documentation: https://github.com/mxcl/homebrew/wiki/Formula-Cookbook
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Openlase < Formula
+  homepage 'https://github.com/edy555/openlase'
+  url ''
+  sha1 ''
+
+  head 'https://github.com/edy555/openlase.git'
+
+  depends_on 'cmake' => :build
+  depends_on 'yasm' => :build
+  #depends_on 'jack'  # Jack OSX http://www.jackosx.com/ is recommended
+  depends_on 'ffmpeg' => :recommended
+  depends_on 'qt' => :optional
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make install"
+  end
+end
